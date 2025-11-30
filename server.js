54b394a28f829ac553246d1067920267b26f3aa2
Sebastian Castro <sebicas@gmail.com>
2025-11-30 14:58:47 -0600

    feat: disable `Accept-Ranges` header for static file serving

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve static files from the current directory
-app.use(express.static(__dirname));
+app.use(express.static(__dirname, { acceptRanges: false }));
 
 // Serve index.html for the root route
 app.get('/', (req, res) => {
